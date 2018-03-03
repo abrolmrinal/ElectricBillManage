ac3fb43c3415465cf94662c4a758d36a8ff99d2e
abrolmrinal <mrinal15062@iiitd.ac.in>
2018-03-04 01:31:25 +0530

    made login main activity

--- app/src/main/java/com/mc/group3/electricbillmanage/LoginActivity.java
+++ app/src/main/java/com/mc/group3/electricbillmanage/LoginActivity.java
@@ -2,22 +2,32 @@ package com.mc.group3.electricbillmanage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View.*;
+import android.view.View;
 import android.widget.*;
 import android.content.Intent;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText loginEmailEdit;
+    private EditText loginPassEdit;
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText loginEmailEdit = findViewById(R.id.loginEmailEdit);
-        EditText loginPassEdit = findViewById(R.id.loginPassEdit);
+        loginEmailEdit = findViewById(R.id.loginEmailEdit);
+        loginPassEdit = findViewById(R.id.loginPassEdit);
+    }
 
-        if(loginEmailEdit.getText().equals("admin") && loginPassEdit.getText().equals("password")){
-            Intent intentToDash = new Intent()
+    public void loginButtonListener(View v){
+        if(loginEmailEdit.getText().toString().equals(getString(R.string.user_email)) && loginPassEdit.getText().toString().equals(getString(R.string.tempPasswordString))){
+            Intent intentToLaunch = new Intent(this, LauncherActivity.class);
+            startActivity(intentToLaunch);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), getString(R.string.LoginFailToast), Toast.LENGTH_LONG).show();
+            loginEmailEdit.setText("");
+            loginPassEdit.setText("");
         }
     }
 }
