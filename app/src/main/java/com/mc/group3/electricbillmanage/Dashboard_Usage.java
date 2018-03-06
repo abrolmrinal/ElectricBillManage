5206f97f40421ae7c37ef8feaea1451f2da7e901
Ishbir Walia <ishbir15041@iiitd.ac.in>
2018-03-06 20:51:52 +0530

    tabDigit(usage page) animates but needs data

--- app/src/main/java/com/mc/group3/electricbillmanage/Dashboard_Usage.java
+++ app/src/main/java/com/mc/group3/electricbillmanage/Dashboard_Usage.java
@@ -3,37 +3,49 @@ package com.mc.group3.electricbillmanage;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import com.xenione.digit.TabDigit;
+
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Dashboard_Usage extends Fragment {
+public class Dashboard_Usage extends Fragment implements Runnable {
 
 
     public Dashboard_Usage() {
         // Required empty public constructor
     }
 
+    TabDigit tabDigit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard_usage, container, false);
+        View view = inflater.inflate(R.layout.fragment_dashboard_usage, container, false);
+
+        ///*
+        tabDigit = (TabDigit) view.findViewById(R.id.tabDigit1);
+        tabDigit.setChar(1);
+        //tabDigit.run();
+        ViewCompat.postOnAnimationDelayed(tabDigit, this, 100);
+
+        return view;
     }
 
     /*
     tabDigit1.start();
     ViewCompat.postOnAnimationDelayed(tabDigit1, this, 1000);
-
+    */
     @Override
     public void run() {
-        tabDigit1.start();
-        ViewCompat.postOnAnimationDelayed(tabDigit1, this, 1000);
-     */
-
+        tabDigit.start();
+        ViewCompat.postOnAnimationDelayed(tabDigit, this, 1000);
+    }
 }
